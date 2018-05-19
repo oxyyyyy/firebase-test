33afe0b251908c792133c7c271c11fda75911405
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2018-05-19 23:14:38 +0300

    hueta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBpNddvcJ-Soy1LMThox2jQC2Tj20zx_Wo',
+    authDomain: 'fir-test-1b334.firebaseapp.com',
+    databaseURL: 'https://fir-test-1b334.firebaseio.com',
+    projectId: 'fir-test-1b334',
+    storageBucket: 'fir-test-1b334.appspot.com',
+    messagingSenderId: '478073807430'
+  }
 };
